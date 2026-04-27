b79705d734bd77221e9e3762dca6409bf9e76ef6
Eivind Siqveland Larsen <eivind@siqve.land>
2026-04-27 21:15:49 +0200

    Update TextUtilsTest.java

--- src/test/java/org/subsound/utils/javahttp/TextUtilsTest.java
+++ src/test/java/org/subsound/utils/javahttp/TextUtilsTest.java
@@ -8,6 +8,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TextUtilsTest {
 
     @Test
+    public void testescapeMarkupGtkLabel() {
+        assertThat(TextUtils.escapeMarkupGtkLabel("foo")).isEqualTo("foo");
+        assertThat(TextUtils.escapeMarkupGtkLabel("foo <bar>")).isEqualTo("foo &lt;bar&gt;");
+        assertThat(TextUtils.escapeMarkupGtkLabel("foo <bar> baz")).isEqualTo("foo &lt;bar&gt; baz");
+        assertThat(TextUtils.escapeMarkupGtkLabel("foo <bar> baz <quux>")).isEqualTo("foo &lt;bar&gt; baz &lt;quux&gt;");
+        assertThat(TextUtils.escapeMarkupGtkLabel("King Geedorah featuring Mr Fantastik & Hassan Chop & DOOM"))
+                .isEqualTo("King Geedorah featuring Mr Fantastik &amp; Hassan Chop &amp; DOOM");
+    }
+
+    @Test
     public void testBiography() {
         assertThat(parseLink("")).isNotNull();
         String sample = "backing vocals <a target='_blank' href=\"https://www.last.fm/music/The+Test+Band\" rel=\"nofollow\">Read more on Last.fm</a>";
